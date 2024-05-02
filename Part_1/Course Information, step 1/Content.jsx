432dc613ac1a627be309b5fdad2716a7764cfef6
SBakalov <SBakalov@technologica.com>
2024-05-02 10:32:44 +0300

    Course Information, step 2

--- Part_1/Course Information, step 1/Content.jsx
+++ Part_1/Course Information, step 1/Content.jsx
@@ -1,13 +1,23 @@
 const Content = (props) => {
-    const maps = props.parts.map(f =>
-        <p key={f.part}>{f.part} {f.exercises}</p >
-    );
+    // const maps = props.parts.map(f =>
+    //     <p key={f.part}>{f.part} {f.exercises}</p >
+    // );
 
     return (
         <div>
-            {maps}
+            <Part part={props.parts[0].part} exercises={props.parts[0].exercises} />
+            <Part part={props.parts[1].part} exercises={props.parts[1].exercises} />
+            <Part part={props.parts[2].part} exercises={props.parts[2].exercises} />
         </div>
     );
 }
 
 export default Content;
+
+const Part = (props) => {
+    return (
+        <div>
+            <p>{props.part} {props.exercises}</p >
+        </div >
+    );
+}
