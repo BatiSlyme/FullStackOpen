794647255805b97d80890a44928fbaed2b90d49e
SBakalov <sbakalov@gmail.com>
2024-05-03 16:54:55 +0300

    1.4: Course Information step 4

--- Part_1/Content.jsx
+++ Part_1/Content.jsx
@@ -1,24 +1,20 @@
 const Content = (props) => {
-    // const maps = props.parts.map(f =>
-    //     <p key={f.part}>{f.part} {f.exercises}</p >
-    // );
+    const maps = props.parts.map((f, i) =>
+        <p key={i}>{f.name} {f.exercises}</p >
+    );
     console.log(props);
     return (
-        <div>
-            <Part part={props.part1.name} exercises={props.part1.exercises} />
-            <Part part={props.part2.name} exercises={props.part2.exercises} />
-            <Part part={props.part3.name} exercises={props.part3.exercises} />
-        </div>
+        maps
     );
 }
 
 export default Content;
 
-const Part = (props) => {
-    console.log(props);
-    return (
-        <div>
-            <p>{props.part} {props.exercises}</p >
-        </div >
-    );
-}
+// const Part = (props) => {
+//     console.log(props);
+//     return (
+//         <div>
+//             <p>{props.part} {props.exercises}</p >
+//         </div >
+//     );
+// }
