475331cf18e1631b875da34a694529f512e2384c
SBakalov <SBakalov@technologica.com>
2024-05-15 16:08:35 +0300

    2.9*: The Phonebook Step 4

--- Part_2/App.jsx
+++ Part_2/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123-12312' }
-  ])
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
   const [newName, setNewName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
-
+  const [filterName, setFilterName] = useState('');
   const submit = (event) => {
     event.preventDefault();
 
@@ -22,22 +25,27 @@ const App = () => {
       setNewName('');
       setPhoneNumber('');
     }
-
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
+    console.log(event.target.value);
+    setNewName(event.target.value);
   }
 
   const handlePhoneNumberChange = (event) => {
-    console.log(event.target.value)
-    setPhoneNumber(event.target.value)
+    console.log(event.target.value);
+    setPhoneNumber(event.target.value);
+  }
+
+  const handleFilterChange = (event) => {
+    setFilterName(event.target.value);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with: <input value={filterName} onChange={handleFilterChange} />
+      <div><h1>add a new</h1></div>
       <form onSubmit={submit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -50,8 +58,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>{persons.map((f, i) => <li key={i}>{f.name} {f.number}</li>)}</ul>
-
+      <ul>{persons.filter(f => {
+        let re = new RegExp(`${filterName}`);
+        if (re.test(f.name.toLocaleLowerCase())) {
+          return f;
+        }
+      }).map((f, i) => <li key={i}>{f.name} {f.number}</li>)}</ul>
     </div>
   )
 }
