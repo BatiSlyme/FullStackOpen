552eac767eefc8efdf66374f1001cdd090595cf8
SBakalov <SBakalov@technologica.com>
2024-05-15 12:08:29 +0300

    2.4: Course information step9 2.5: Separate module step10

--- Part_2/Course.tsx
+++ Part_2/Course.tsx
@@ -1,13 +1,20 @@
-const Course = ({ course }) => {
-    console.log(course);
+const Course = ({ courses }) => {
+    console.log('courses', courses);
 
     return (
-        course.parts.map(f =>
-            <div key={f.id}>
-                {f.name} {f.exercises}
-            </div>
-        )
+        courses.map(f => {
+            console.log(f);
+            return (
+                <div key={f.id}>
+                    <h1>{f.name}</h1>
+                    {f.parts.map((p, i) => <div key={i}>{p.name} {p.exercises}</div>)}
+                    {'total of ' + f.parts.reduce((sum, order) => sum + order.exercises, 0) + ' exercises'}
+                </div >
+            );
+
+        })
     );
+
 
 }
 
