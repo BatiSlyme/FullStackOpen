07732d6615050be1a19fff2520ea5e966db8549e
SBakalov <SBakalov@technologica.com>
2024-06-11 10:19:49 +0300

    removing cors

--- part3/index.js
+++ part3/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 // app.use(express.json());
 // app.use(express.static('dist'));
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-app.use(cors());
+// app.use(cors());
 let persons = [
     {
         "id": 1,
