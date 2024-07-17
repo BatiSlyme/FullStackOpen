be2eac452f6ad81b43af11b740ce7add1946cf0a
SBakalov <SBakalov@technologica.com>
2024-07-17 17:10:30 +0300

    fixing ui

--- part3/index.js
+++ part3/index.js
@@ -122,7 +122,7 @@ app.post('/api/persons', (req, res, next) => {
     });
 
     person.save().then(savedPerson => {
-        res.json(savedPerson)
+        res.json(savedPerson);
     }).catch(error => next(error));
 
 });
