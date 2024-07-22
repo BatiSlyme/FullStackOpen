540c2f359edf41a6d63c075c5dda6249d874bfe6
SBakalov <SBakalov@technologica.com>
2024-07-22 15:45:45 +0300

    3.22: Lint configuration

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,51 +1,51 @@
 const { mongoose } = require('mongoose')
 
-const url = process.env.MONGODB_URL;
+const url = process.env.MONGODB_URL
 console.log('connecting to', url)
 
-const name = process.argv[3];
-const number = process.argv[4];
+// const name = process.argv[3]
+// const number = process.argv[4]
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB');
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message);
-    });
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Name required'],
-        minlength: 3,
+  name: {
+    type: String,
+    required: [true, 'Name required'],
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    required: [true, 'Phone number required'],
+    validate: {
+      validator: function (v) {
+        if (v.length < 8) {
+          return false
+        }
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        required: [true, 'Phone number required'],
-        validate: {
-            validator: function (v) {
-                if (v.length < 8) {
-                    return false;
-                }
-                return /^\d{2,3}-\d+$/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        length: 8,
+    length: 8,
 
-    },
-});
+  },
+})
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-});
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 // const Person = mongoose.model('Person', personSchema);
 
@@ -70,7 +70,7 @@ personSchema.set('toJSON', {
 //     });
 // }
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
 
 // Note.find({ important: true }).then(result => {
 //     // ...
